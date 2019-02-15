0b697c0ea4b5dc6f2d0596d7a1f19e4cc505e1b6
venkat-99 <Venkatarao.Kurapati@thomsonreuters.com>
2019-02-15 11:50:37 +0530

    This is first commit

---
+++ test.py
@@ -0,0 +1,121 @@
+"""a=int(input("Enter first number:"))
+b=int(input("Enter Second number:"))
+c=int(input("Enter third number:"))
+max= a if a>b and a>c else b if b>c else c
+printprint("Maximum Value is:",max)"""
+
+
+"""x=input("Enter first number:")
+y=input("Enter second number:")
+v=int(x)
+k=int(y)
+print("sum:",v+k)"""
+
+
+
+"""a,b={int(x) for x in input("Enter 2 numbers:").split()}
+print("sum of 2 values is:",a+b)"""
+
+"""x=eval(input("Enter some num:"))
+print(type(x))
+for x1 in x:
+  print(x1)"""
+  
+  
+
+"""a,b,c=[eval(x) for x in input("Enter 3 num:").split(",")]
+print(type(a))
+print(type(b))
+print(type(c))"""
+
+
+#from sys import argv
+#print(type(argv))
+#print(argv)
+#print(argv[1:])
+#print("print length of CMD line arguments:",len(argv))
+
+"""from sys import argv
+args=argv[1:]
+sum=0
+for x in args:
+   a=eval(x)
+   sum +=a
+print("The sum:",sum) """
+
+
+"""from sys import argv
+print(eval(argv[1])+eval(argv[2]))"""
+
+"""print(10,20,30,40,sep=",",end="...")
+print(50,60,70,80,sep="-")"""
+
+"""name="Venkat"
+lastname="'kurapati'"
+salary=30000
+print("Hello {} tell me u r last name {} and ur monthly income {}".format(name,lastname,salary))
+print("Hello {0} tell me u r last name {1} and ur monthly income {2}".format(name,salary,lastname))
+print("Hello {x} tell me u r last name {y} and ur monthly income {z}".format(x=name,z=salary,y=lastname))"""
+
+
+ 
+"""v=input("Enter some string:")
+i=0
+for x in v:
+  if i!=0 and i!=1 and i!=2:
+     print("the charecter present at",i,"index of",x)  
+  i+=1"""
+
+ 
+"""for x in range(8):
+  print("venkat")"""
+  
+
+"""for x in range(45):
+  if x%2!=0:
+    print(x)"""
+   
+"""for x in range(10,0,-1)"""
+
+"""l=eval(input("Enter the list:"))
+a=0
+for x in l:
+  a+=x
+print("the sum:",a)"""
+
+"""x=10
+while x<=20:
+ print()
+ x+=1"""
+
+"""n=int(input("Enter some number:"))
+sum=0
+k=1
+while k<=n:
+  sum=sum+k
+  k+=1
+print("The sum of first",n,"numbers is:",sum)"""
+
+"""name=""
+pwd=""
+while name!="kurapati" or pwd!="python":
+    name=input("Enter name:")
+    pwd=input("Enter pwd:")
+print("Hello kurapati thanks for the confirmation")"""
+
+n=int(input("Enter the number of row:")
+for i in range 
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
+   
