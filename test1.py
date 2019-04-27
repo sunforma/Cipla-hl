f9b334825ec927f9c1e5853930aac3b6cfe2dcbe
venkat-99 <Venkatarao.Kurapati@thomsonreuters.com>
2019-04-27 10:17:37 +0530

    maven

---
+++ test1.py
@@ -0,0 +1,16 @@
+"""for i in range(4):
+   for j in range(4):
+     print("i={} and j={}".format(i,j))"""
+
+
+"""n=int(input("Enter the number:"))
+for i in range(1,n+1):
+  for j in range(i):
+   print("*", end=" ")
+  print()"""
+
+n=int(input("Enter the num:"))
+for i in range(n):
+  for j in range(n):
+    print("*"*n)
+  print()	
